eb20093eea2ba3d181d246314f5283697a50e6d1
Francis230 <francisjc230@gmail.com>
2025-05-06 16:33:36 -0500

    Formulario guardado en Firebase

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  formulario!: FormGroup;
+  constructor(private fb: FormBuilder, private firestore: Firestore) {}
+  // Los campos permitidos par llenr la informacion que se va a realizar 
+  ngOnInit() {
+    this.formulario = this.fb.group({
+      nombre: [''],
+      apellido: [''],
+      email: [''],
+      fechaNacimiento: [''],
+      comida: [''],
+      equiposfutbol: [[]],
+      telefono: [''],
+      cualidad: ['']
+    });
+  }
+  // Accion de guardar la informacion registrada
+  async guardar() {
+    const datos = this.formulario.value;
+    const ref = collection(this.firestore, 'formularios');
+    await addDoc(ref, datos);
+    // Alerta para poder notificar de que los datos se guardaron
+    alert('Datos guardados correctamente');
+    this.formulario.reset();
+  }
 }
+
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,26 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // firebaseConfig : {
+  //   apiKey: "AIzaSyByQgiCUzf3gYfFYcw4Pl7Rec-yRFjSIZA",
+  //   authDomain: "a-e2689.firebaseapp.com",
+  //   projectId: "a-e2689",
+  //   storageBucket: "a-e2689.firebasestorage.app",
+  //   messagingSenderId: "941773529283",
+  //   appId: "1:941773529283:web:8a9199b937e0fe88a88276",
+  //   measurementId: "G-PB8QCM0HKG"
+  // }
+
+  firebaseConfig : {
+    apiKey: "AIzaSyDxXAmZaRmBjfa0OBK2rmLeT2eCJcd64w0",
+    authDomain: "appmovilfirebase-1079d.firebaseapp.com",
+    projectId: "appmovilfirebase-1079d",
+    storageBucket: "appmovilfirebase-1079d.firebasestorage.app",
+    messagingSenderId: "390040257502",
+    appId: "1:390040257502:web:23cbb6d96cd55365a3934f",
+    measurementId: "G-N7Z5NWRSXE"
+  }
 };
 
 /*
